8502dfe3a3e752035a1f6afc6eda81c61c4b5b34
gautamhazarika <gautamhazarika01@gmail.com>
2025-04-18 14:56:12 +0530

    Added Complete Landing Page UIs

---
+++ frontend/src/components/login-form.tsx
@@ -0,0 +1,79 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+interface LoginFormProps extends React.ComponentProps<"form"> {
+  onGoogleLogin: () => void
+  onForgotPassword: () => void
+  onSignUp: () => void
+  label: string
+}
+
+export function LoginForm({
+  className,
+  onGoogleLogin,
+  onForgotPassword,
+  onSignUp,
+  label = "Email or Student ID",
+  ...formProps
+}: LoginFormProps) {
+
+
+  
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...formProps}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your info below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="text">{label}</Label>
+          <Input id="email" type="text" placeholder="me@example.com or john_doe123" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                onForgotPassword();
+              }}
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+          <span className="bg-background text-muted-foreground relative z-10 px-2">
+            Or continue with
+          </span>
+        </div>
+        <Button variant="outline" className="w-full" onClick={onGoogleLogin}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="20px" height="20px" className="mr-2">
+            <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z" />
+          </svg>
+          Log in with Google
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <a href="#" className="underline underline-offset-4" onClick={(e) => {
+          e.preventDefault();
+          onSignUp();
+        }}>
+          Sign up
+        </a>
+      </div>
+    </form>
+  )
+}
